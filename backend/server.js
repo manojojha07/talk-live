9292a48317a52ab382a7eb2aee3352b607be527e
manoj-ojha04 <manojojha154@gmail.com>
2026-03-01 15:31:41 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import 'dotenv/config'
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req , res) => {
+res.status(200).json({msg:"server live backend 123 !"});
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`);
+})
