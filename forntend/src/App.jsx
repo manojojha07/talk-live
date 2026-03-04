f4aa77b3c85b3ad6d681d0545b07e4d141d30acf
manoj-ojha04 <manojojha154@gmail.com>
2026-03-04 14:46:56 +0530

    first

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -1,11 +1,26 @@
-import { SignIn, SignInButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton } from '@clerk/clerk-react'
 import React from 'react'
 
 const App = () => {
   return (
     <div>
       <h1>Welcome to our web Application</h1>
-      <SignInButton mode='modal'/>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">
+            Sign In
+          </button>
+        </SignInButton>
+      </SignedOut>
+
+      {/* Jab user login hai */}
+      <SignedIn>
+        <SignOutButton >
+          <button className="bg-red-500 text-white px-4 py-2 rounded">
+            Logout
+          </button>
+        </SignOutButton>
+      </SignedIn>
     </div>
   )
 }
