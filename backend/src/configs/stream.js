111241c1b4ac2afeaba0ded43438be5c9295abe5
manoj-ojha04 <manojojha154@gmail.com>
2026-03-07 14:07:00 +0530

    strem function add

---
+++ backend/src/configs/stream.js
@@ -0,0 +1,33 @@
+import  { StreamChat } from 'stream-chat';
+import { ENV } from './env.js';
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    console.log("STREAM_API_KEY ||  STREAM_API_SECRET is missing!");  
+}
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async(userData) =>{
+    try {
+        await chatClient.upsertUser(userData)
+        console.log("Sream user upserted successfully: ", userData);
+        return userData;
+    } catch (error) {
+        console.log("stream function problam error!"); 
+    }
+}
+
+
+export const deleteStreamUser = async(userId) =>{
+    try {
+        await chatClient.deleteUser(userId)
+        console.log("Sream user delte successfully: ", userId);
+        
+    } catch (error) {
+        console.log("stream function delete problam error!"); 
+    }
+}
+
+// todo : add another method to Genration token
